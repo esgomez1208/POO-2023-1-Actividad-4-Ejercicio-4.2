3866a5812826a342eacde471523c508ae5f7d542
Esteban Gómez Benítez <69543140+esgomez1208@users.noreply.github.com>
2023-06-08 00:46:55 -0500

    Source Code
    Inmuebles Ejercicio 4.2

---
+++ Inmuebles/src/inmuebles/InmuebleVivienda.java
@@ -0,0 +1,19 @@
+package inmuebles;
+
+public class InmuebleVivienda extends Inmueble {
+    
+    protected int numeroHabitaciones;
+    protected int numeroBaños;
+    
+    public InmuebleVivienda(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBaños) {
+        super(identificadorInmobiliario, area, direccion);
+        this.numeroHabitaciones = numeroHabitaciones;
+        this.numeroBaños = numeroBaños;
+    }
+    
+    void imprimir() {
+        super.imprimir();
+        System.out.println("Numero de habitaciones = " + numeroHabitaciones);
+        System.out.println("Numero de baños = " + numeroBaños);
+    } 
+}
